127bbe56af3d18f3dfd7e74e9b48243a49759352
Andras Wirth <agrippa.wia@gmail.com>
2020-03-16 17:30:23 +0100

    Init

---
+++ src/main/java/hu/domokosdaniel56/cosy/CosyApplication.java
@@ -0,0 +1,13 @@
+package hu.domokosdaniel56.cosy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/domokosdaniel56/cosy/CosyApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.domokosdaniel56.cosy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
